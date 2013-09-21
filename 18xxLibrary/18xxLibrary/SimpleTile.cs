800841952d4f261eb5b44f0a818f14d5df3048bf
Jonathan <physicssquirrel@gmail.com>
2013-09-21 02:56:21 -0700

    Made simpletile more readable and implemented super awesome HexTile xaml class

--- 18xxLibrary/18xxLibrary/SimpleTile.cs
+++ 18xxLibrary/18xxLibrary/SimpleTile.cs
@@ -8,18 +8,17 @@ namespace TrainEngineLibrary
 {
     class SimpleTile : Tile
     {
-        private readonly int exits;
+        private readonly HashSet<int> exits;
 
         public SimpleTile(params int[] edges)
         {
             if (edges.Count() > 6) throw new ArgumentException("Only six or fewer edges may be provided");
-            int exits = 0;
+            exits = new HashSet<int>();
             foreach (int edge in edges)
             {
                 if (edge > 5 || edge < 0) throw new ArgumentException("Valid arguments must be in the range 0-5", "edges");
-                exits |= 1 << edge;
+                exits.Add(edge);
             }
-            this.exits = exits;
         }
 
         public override bool UpgradesTo(Tile tile)
@@ -27,17 +26,27 @@ namespace TrainEngineLibrary
             if ((Id != null && tile.Id != null) || !(tile is SimpleTile)) return base.UpgradesTo(tile);
 
             SimpleTile other = (SimpleTile)tile;
-            return (this.exits & other.exits) == this.exits && other.Phase == this.Phase + 1;
+            return (this.exits.IsSubsetOf(other.exits)) && other.Phase == this.Phase + 1;
         }
 
         public override bool RouteExists(int edgeA, int edgeB)
         {
-            return (exits << edgeA & exits << edgeB & 1) == 1;
+            return exits.Contains(edgeA) && exits.Contains(edgeB);
         }
 
         public override bool TrackExists(int edge)
         {
-            return (exits << edge & 1) == 1;
+            return exits.Contains(edge);
+        }
+
+        public override HashSet<int> GetRouteConnections(int edge)
+        {
+
+            if(!exits.Contains(edge)) return new HashSet<int>();
+            HashSet<int> result = new HashSet<int>(exits);
+            result.Remove(edge);
+            return result;
+            
         }
     }
 }
--- 18xxLibrary/BoardViewer/HexTile.xaml.cs
+++ 18xxLibrary/BoardViewer/HexTile.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,176 @@ namespace BoardViewer
     /// </summary>
     public partial class HexTile : UserControl
     {
+        protected const int Z_WHITE_CIRCLE =     0;
+        protected const int Z_WHITE =            1;
+        protected const int Z_CROSSING_BLACK =   2;
+        protected const int Z_CROSSING_WHITE =   3;
+        protected const int Z_BLACK =            4;
+        protected const int Z_BLACK_CIRCLE =     5;
+
         public HexTile()
         {
             InitializeComponent();
         }
+
+        #region Paths
+        private String _paths;
+
+        public String Paths
+        {
+            get
+            {
+                return _paths;
+            }
+
+            set
+            {
+                String[] pairs = value.Split(' ');
+                List<UIElement> whites = new List<UIElement>();
+                List<UIElement> blacks = new List<UIElement>();
+                foreach (String pairString in pairs)
+                {
+                    if (pairString == "") continue;
+                    String[] pair = pairString.Split(',');
+                    Tuple<Path,Path> both = GetPathFromEndPoints(pair[0].ToCharArray()[0], pair[1].ToCharArray()[0]);
+                    whites.Add(both.Item1);
+                    blacks.Add(both.Item2);
+                }
+
+                Tuple<int,int> crossed = FindIntersectingPaths(pairs);
+                if (crossed != null)
+                {
+                    Grid.SetZIndex(whites[crossed.Item1], Z_CROSSING_WHITE);
+                    Grid.SetZIndex(blacks[crossed.Item2], Z_CROSSING_BLACK);
+                }
+
+                PathGrid.Children.Clear();
+
+                if (value.Contains('c'))
+                {
+                    Ellipse ew = new Ellipse();
+                    ew.Fill = new SolidColorBrush(Colors.White);
+                    ew.Width = ew.Height = 30;
+                    ew.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                    ew.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                    ew.Margin = new Thickness(-15,-15,0,0);
+                    Grid.SetZIndex(ew, Z_WHITE_CIRCLE);
+                    whites.Insert(0, ew);
+                    Ellipse eb = new Ellipse();
+                    eb.Fill = new SolidColorBrush(Colors.Black);
+                    eb.Width = eb.Height = 20;
+                    eb.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                    eb.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                    eb.Margin = new Thickness(-10,-10,0,0);
+                    Grid.SetZIndex(eb, Z_BLACK_CIRCLE);
+                    blacks.Add(eb);
+                }
+
+                foreach (UIElement e in whites)
+                {
+                    PathGrid.Children.Add(e);
+                }
+                foreach (UIElement e in blacks)
+                {
+                    PathGrid.Children.Add(e);
+                }
+                _paths = value;
+            }
+        }
+        #endregion
+
+        public Color Color
+        {
+            get { return ((SolidColorBrush)TileBackground.Fill).Color; }
+            set { TileBackground.Fill = new SolidColorBrush(value); }
+        }
+
+        public Int64 Rotation
+        {
+            get { return (((Int64)((RotateTransform)RenderTransform).Angle)-30)/60; }
+            set { RenderTransform = new RotateTransform(value*60+30); }
+        }
+
+        private Tuple<Path,Path> GetPathFromEndPoints(char c1, char c2)
+        {
+            char start = c1 < c2 ? c1 : c2;
+            char finish = c1 < c2 ? c2 : c1;
+            String data;
+            if (finish <= '5') // both edges
+            {
+                data = String.Format(sPath[finish - start], sPts[start - '0'], sPts[finish - '0']);
+            }
+            else if (finish == 'c') // center of the map
+            {
+                data = String.Format(sPath[3], sPts[start - '0'], "0,0");
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+            
+            Path bg = new Path();
+            Path fg = new Path();
+            bg.SetValue(Path.DataProperty, Geometry.Parse(data));
+            fg.SetValue(Path.DataProperty, Geometry.Parse(data));
+            bg.SetValue(Path.StrokeProperty, new BrushConverter().ConvertFromString("White"));
+            fg.SetValue(Path.StrokeProperty, new BrushConverter().ConvertFromString("Black"));
+            bg.SetValue(Path.StrokeThicknessProperty, 30.0);
+            fg.SetValue(Path.StrokeThicknessProperty, 20.0);
+            Grid.SetZIndex(bg, Z_WHITE);
+            Grid.SetZIndex(fg, Z_BLACK);
+            return new Tuple<Path, Path>(bg, fg);
+        }
+
+        private Tuple<int, int> FindIntersectingPaths(String[] pairs)
+        {
+            List<Tuple<int, int, int>> segments = new List<Tuple<int, int, int>>();
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                String pairString = pairs[i];
+                String[] pair = pairString.Split(',');
+                char a = pair[0].ToCharArray()[0];
+                char b = pair[1].ToCharArray()[0];
+                if (a > '5' || b > '5') segments.Add(new Tuple<int, int, int>('5', '5', i));
+                else segments.Add(a < b ? new Tuple<int, int, int>(a, b, i) : new Tuple<int, int, int>(b, a, i));
+            }
+            segments.Sort((x, y) =>
+                { if (x.Item1 > y.Item1) return 1;
+                  if (x.Item1 < y.Item1) return -1;
+                  if (x.Item2 > y.Item2) return 1;
+                  if (x.Item2 < y.Item2) return -1;
+                  return 0;
+                });
+            for (int i = 0; i < segments.Count; i++)
+            {
+                for (int j = i + 1; j < segments.Count; j++)
+                {
+                    if (segments[i].Item1 < segments[j].Item1
+                                         && segments[j].Item1 < segments[i].Item2
+                                                             && segments[i].Item2 < segments[j].Item2)
+                        return new Tuple<int, int>(segments[i].Item3, segments[j].Item3);
+                }
+            }
+            return null;
+        }
+
+        private String[] sPath =
+            // M startPoint A size rotationAngle 0 0 endPoint
+            { "",
+              "M {0} A 100,100 0 0 0 {1}",
+              "M {0} A 300,300 0 0 0 {1}",
+              "M {0} L {1}",
+              "M {1} A 300,300 0 0 0 {0}",
+              "M {1} A 100,100 0 0 0 {0}"
+            };
+
+        private String[] sPts =
+            { "0,175",
+              "-150,87",
+              "-150,-87",
+              "0,-175",
+              "150,-87",
+              "150,87"
+            };
     }
 }
